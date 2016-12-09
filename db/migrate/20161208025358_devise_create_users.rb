135e34f56deb2dcc5ed672d92aa00cbe2516f500
Ryan Kliemann <Ryan_blake@icloud.com>
2016-12-09 10:52:00 -0600

    name_table

--- db/migrate/20161208025358_devise_create_users.rb
+++ db/migrate/20161208025358_devise_create_users.rb
@@ -2,6 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -19,10 +20,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string     :current_sign_in_ip
       t.string     :last_sign_in_ip
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
+      # Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
