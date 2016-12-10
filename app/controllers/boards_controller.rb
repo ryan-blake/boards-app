c8dcf86622d1b2e3ece16e7e50281ce7c4ef0932
Ryan Kliemann <Ryan_blake@icloud.com>
2016-12-09 18:30:41 -0600

    signed_on

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,9 +6,11 @@ class BoardsController < ApplicationController
   def index
     @boardies = Board.all
     # make boardies be where board.boolean == false and board.arrived = false
-    @on = current_user
-    @charge = Charge.where(user_id: @on.id)
+    if signed_on
+      @on = current_user
+      @charge = Charge.where(user_id: @on.id)
       @boards = Board.find_by(title: @charge.item )
+      end
     end
 
 
