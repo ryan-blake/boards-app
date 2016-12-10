106684ae9a000e7246abc4ad14c48f5722a0d520
Ryan Kliemann <Ryan_blake@icloud.com>
2016-12-09 18:36:23 -0600

    .find

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,7 +9,7 @@ class BoardsController < ApplicationController
     if current_user.present?
       @on = current_user
       @charge = Charge.where(user_id: @on.id)
-      @boards = Board.find_by(title: @charge.item )
+      @boards = Board.find(title: @charge.item )
       end
     end
 
