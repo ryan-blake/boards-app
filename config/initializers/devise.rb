00b9bf271fcee367f005222188848db35e8148ff
Ryan Kliemann <Ryan_blake@icloud.com>
2016-12-19 13:01:38 -0600

    previous-try

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,7 +254,7 @@ Devise.setup do |config|
     :scope => 'read_write',
     :stripe_landing => 'register'
 
-  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'] ,
+  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET']
 
 
   # ==> Warden configuration
