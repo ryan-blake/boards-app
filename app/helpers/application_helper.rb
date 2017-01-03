bfb905f0bb96e7a7fc639f19a3e72f9184ac1e6d
Ryan Kliemann <Ryan_blake@icloud.com>
2017-01-02 22:28:21 -0600

    update filtering to work with ajax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ private
 def sortable(column, title = nil)
   title ||= column.titleize
   direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-  link_to title, params.merge(:sort => column, :direction => direction, :page => nil).permit!
+  link_to title, params.merge(:sort => column, :direction => direction, :page => nil).permit!, remote: true
 
 
 end
