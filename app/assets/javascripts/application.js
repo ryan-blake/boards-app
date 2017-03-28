784da86b9d7866f396cc0245e8f9883ec8c33cfe
Ryan Kliemann <Ryan_blake@icloud.com>
2017-03-28 11:30:35 -0500

    reinsert Hide Show for shipping partials, target: pagination for index on load; too many records.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,7 +36,22 @@ $(document).ready(function() {
     }
   });
 });
+// shipping hidden charge form
+$(document).ready(function() {
+var checkbox = document.getElementById('checkbox');
+var delivery_div = document.getElementById('delivery');
+var pickup_div = document.getElementById('pickup');
 
+checkbox.onclick = function() {
+   if(this.checked) {
+     delivery_div.style['display'] = 'block';
+     pickup_div.style['display'] = 'none';
+   } else {
+     pickup_div.style['display'] = 'block';
+     delivery_div.style['display'] = 'none';
+   }
+};
+});
 
 (function() {
   $(document).on('click', '.toggle-window', function(e) {
