e6d9643d86f519a1b042f5da18c6edf324b4189c
Ryan Kliemann <Ryan_blake@icloud.com>
2017-03-30 17:54:04 -0500

    remove pagination from active boards userdash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
+  helper_method :sort_column, :sort_direction
   def list
     @users = User.all
     # @boards = Board.all
     @boardies = Board.all
     @charge  = Charge.new
   end
-
-
 
   def boards
     @user = current_user
@@ -20,5 +19,15 @@ class PagesController < ApplicationController
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages)
       .find(session[:conversations])
+  end
+
+private
+
+  def sort_column
+    Board.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "desc"
   end
 end
