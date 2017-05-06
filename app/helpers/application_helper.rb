2c87788912221fd7961c46acdc61ffa66248c791
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-06 15:55:16 -0500

    AS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,13 @@ module ApplicationHelper
   @types_search = Type.all
   @distances_search = Distance.all
 
+  def format_amount(amount)
+    sprintf('$%0.2f', amount.to_f / 100.0).gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
+  end
 
+  def format_date(created)
+    Time.at(created).getutc.strftime("%m/%d/%Y")
+  end
 
 private
 
