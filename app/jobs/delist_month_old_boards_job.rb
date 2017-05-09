3b3a3e6b1f463a4db53bdcd16b9503bc3b4b3462
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-09 17:03:52 -0500

    s

--- app/jobs/delist_month_old_boards_job.rb
+++ app/jobs/delist_month_old_boards_job.rb
@@ -3,7 +3,7 @@ class DelistMonthOldBoardsJob < ApplicationJob
 
   def perform(*args)
     a = []
-    boards = Board.where(:for_sale => true).group_by_day(:updated_at, range: 4.weeks.ago.midnight..Time.now)
+    boards = Board.where(:for_sale => true).group_by_day(:updated_at, range: 3.weeks.ago.midnight..Time.now)
     boards.each do |i|
       i.for_sale = false
       i.list_time = ""
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20170509170935) do
 
   create_table "boards", force: :cascade do |t|
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.string   "make"
     t.boolean  "used"
     t.integer  "price"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20170509170935) do
     t.boolean  "shipped"
     t.string   "tracking"
     t.boolean  "rental",      default: false
-    t.datetime "list_time"
+    t.datetime "list_time",   default: '2017-05-09 21:56:54'
     t.index ["category_id"], name: "index_boards_on_category_id"
     t.index ["type_id"], name: "index_boards_on_type_id"
     t.index ["user_id"], name: "index_boards_on_user_id"
