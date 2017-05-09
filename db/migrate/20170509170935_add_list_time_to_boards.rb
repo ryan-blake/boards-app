44eac7f1f10ca92b81be1ceccb46db6e3309f1e8
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-09 16:27:48 -0500

    datetime

--- db/migrate/20170509170935_add_list_time_to_boards.rb
+++ db/migrate/20170509170935_add_list_time_to_boards.rb
@@ -1,5 +1,5 @@
 class AddListTimeToBoards < ActiveRecord::Migration[5.0]
   def change
-    add_column :boards, :list_time, :datetime, default: ""
+    add_column :boards, :list_time, :datetime, default: Time.now
   end
 end
