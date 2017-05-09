7693343830d42c78cc6b0d01131b0f97ef6c4f2e
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-09 11:27:28 -0500

    S

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,20 @@
+require 'sidekiq/web'
+
 Sidekiq.configure_server do |config|
-
-    config.redis = {url: ENV['REDIS_PROVIDER']}
-
-  schedule_file = "config/schedule.yml"
-  if File.exists?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
+  ActiveRecord::Base.configurations[Rails.env.to_s]['pool'] = 30
 end
 
-Sidekiq.configure_client do |config|
+if Rails.env.production?
 
-  config.redis = { url: ENV['REDIS_PROVIDER'] }
+   Sidekiq.configure_server do |config|
+   config.redis = { url: ENV["REDISTOGO_URL"]}
+   schedule_file = "config/schedule.yml"
+   if File.exists?(schedule_file)
+     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+   end
+  end
 
+  Sidekiq.configure_client do |config|
+   config.redis = { url: ENV["REDISTOGO_URL"]}
+  end
 end
