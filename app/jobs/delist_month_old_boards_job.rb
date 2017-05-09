8f2747c00f33d7e89ab1eab57d317f93710cefdf
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-09 18:59:47 -0500

    worker-range

--- app/jobs/delist_month_old_boards_job.rb
+++ app/jobs/delist_month_old_boards_job.rb
@@ -3,7 +3,7 @@ class DelistMonthOldBoardsJob < ApplicationJob
 
   def perform(*args)
     a = []
-    boards = Board.where(:for_sale => true).group_by_day(:list_time, range: 3.weeks.ago.midnight..Time.now)
+    boards = Board.where(:for_sale => true).group_by_day(:list_time, range: 20.days.ago.midnight..30.days.ago.midnight)
     boards.each do |i|
       i.for_sale = false
       i.list_time = ""
