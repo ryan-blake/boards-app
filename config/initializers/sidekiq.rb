fb35056650ec206eb5d4c1cc35039ab1e711b768
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-09 00:16:34 -0500

    s

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
-
+  if Rails.env.development?
+    config.redis = { url: 'redis://localhost:6379/0'  }
+  elsif Rails.env.production?
     congif.redis = {url: ENV['REDIS_PROVIDER']}
-
+  end
   schedule_file = "config/schedule.yml"
   if File.exists?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
@@ -10,6 +12,6 @@ end
 
 Sidekiq.configure_client do |config|
 
-  congif.redis = {url: ENV['REDIS_PROVIDER']}
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 
 end
