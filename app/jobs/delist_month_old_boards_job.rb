7cd2c77feecef1fc685fabba1f4def2715d1da3a
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-09 19:31:12 -0500

    S

--- app/jobs/delist_month_old_boards_job.rb
+++ app/jobs/delist_month_old_boards_job.rb
@@ -3,10 +3,10 @@ class DelistMonthOldBoardsJob < ApplicationJob
 
   def perform(*args)
     a = []
-    boards = Board.where(:for_sale => true)
+    boards = Board.where(:for_sale => false)
     boards.each do |i|
-      i.for_sale = false
-      i.list_time = ""
+      i.for_sale = true
+      i.list_time = [Time.now, Time.now - 2.weeks, Time.now - 4.weeks].sample
       i.save
     end
 
