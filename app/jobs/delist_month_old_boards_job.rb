baa065bbcb22f6bdb28dc30c423e65fb8d69aeb7
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-09 19:45:33 -0500

    crontab

--- app/jobs/delist_month_old_boards_job.rb
+++ app/jobs/delist_month_old_boards_job.rb
@@ -4,7 +4,7 @@ class DelistMonthOldBoardsJob < ApplicationJob
   def perform(*args)
     a = []
     boards = Board.where(:for_sale => true)
-    boards = boards.where(['list_time < ?', 10.days.ago])
+    boards = boards.where(['list_time < ?', 30.days.ago])
     boards.each do |i|
       i.for_sale = false
       i.list_time = " "
