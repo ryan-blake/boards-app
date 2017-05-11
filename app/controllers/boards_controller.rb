829019382ea8bff7cdf871e73cd09dd4188dcd6b
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-11 11:54:46 -0500

    ok

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -269,8 +269,10 @@ transactions = Stripe::BalanceTransaction.all(
              if params[:minimum][0].to_i >= 1 && params[:maximum][0].to_i >= 1
                @boards  = @boards.min_length_search(params[:minimum][0].to_i).max_length_search(params[:maximum][0].to_i)
              else
-               if params[:maximum][0].to_i == 12
+               if params[:maximum][0].to_i == 0 && params[:maximum][0].to_i != 0
                  @boards  = @boards.min_length_search(:minimum[0].to_i).max_length_search(9999)
+               elsif params[:minimum][0].to_i == 0 && params[:maximum][0].to_i != 0
+                 @boards  = @boards.min_length_search(9999).max_length_search(:maximum[0].to_i)
                end
              end
 
