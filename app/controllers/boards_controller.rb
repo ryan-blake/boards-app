20c0132607a97198c508c292bcb01c1074df69f5
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-11 11:42:32 -0500

    S

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -269,11 +269,11 @@ transactions = Stripe::BalanceTransaction.all(
              if params[:minimum][0].to_i >= 1 && params[:maximum][0].to_i >= 1
                @boards  = @boards.min_length_search(params[:minimum][0].to_i).max_length_search(params[:maximum][0].to_i)
              else
-               if params[:maximum][0].to_i == 0
+               if params[:maximum][0].to_i <= 1 && params[:minimum][0].to_i >=1 1
                  @boards  = @boards.min_length_search(:minimum[0].to_i).max_length_search(9999)
                else
-               @boards  = @boards.min_length_search(1).max_length_search(params[:maximum][0].to_i)
-             end
+                 @boards  = @boards.min_length_search(1).max_length_search(:maximum[0].to_i)
+               end
              end
 
              if params[:rental] == "on"
