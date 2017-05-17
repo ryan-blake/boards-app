ad35cffceb5df1e9a309b37033e62898cce70c1a
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-17 12:31:57 -0500

    D

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.geocomplete
 //= require moment
+//= require gmaps/google
 //= require bootstrap-datetimepicker
 //= require refile
 //= require lightbox
