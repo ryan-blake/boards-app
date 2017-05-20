85b81549ec3f478fa9760f80a2174337c2d8876e
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-20 11:29:40 -0500

    s-p-conf-key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,6 @@ require "stripe"
 Rails.configuration.stripe = {
   :publishable_key => ENV["STRIPE_PUBLISHABLE_KEY"],
   :secret_key      => ENV["STRIPE_API_KEY"]
-
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
