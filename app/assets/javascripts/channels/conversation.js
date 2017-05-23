2733427a0781d6247e95a1a703db3979bbe1e8f7
Ryan Kliemann <Ryan_blake@icloud.com>
2017-05-23 12:26:16 -0500

    c-panel

--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -3,17 +3,17 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
   disconnected: function() {},
   received: function(data) {
     var conversation = $('#conversations-list').find("[data-conversation-id='" + data['conversation_id'] + "']");
-    var alertMsg = document.getElementById("new-alert");
 
     if (data['window'] !== undefined) {
       var conversation_visible = conversation.is(':visible');
+      //
 
+      //
       if (conversation_visible) {
-
         var messages_visible = (conversation).find('.panel-body').is(':visible');
 
         if (!messages_visible) {
-          conversation.removeClass('panel-default').addClass('panel-success');
+
         }
         conversation.find('.messages-list').find('ul').append(data['message']);
       }
@@ -26,8 +26,6 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
     }
     else {
       conversation.find('ul').append(data['message']);
-      alertMsg.style.display.is(':visible');
-      
     }
 
     var messages_list = conversation.find('.messages-list');
