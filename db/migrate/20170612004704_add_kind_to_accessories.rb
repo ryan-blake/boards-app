454d6495292c40986102a0ca945d929ebcc8a67c
Ryan Kliemann <Ryan_blake@icloud.com>
2017-06-11 22:18:43 -0500

    acc/resultsInSearch

--- db/migrate/20170612004704_add_kind_to_accessories.rb
+++ db/migrate/20170612004704_add_kind_to_accessories.rb
@@ -2,6 +2,5 @@ class AddKindToAccessories < ActiveRecord::Migration[5.0]
   def change
     add_reference :accessories, :kind, index: true, foreign_key: true
     add_reference :kinds, :category, index: true, foreign_key: true
-
   end
 end
