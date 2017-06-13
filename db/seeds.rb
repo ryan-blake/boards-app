85bef39041e2c5a140a36379f90c476d2df19db3
Ryan Kliemann <Ryan_blake@icloud.com>
2017-06-13 13:46:35 -0500

    seed-imgs

--- db/seeds.rb
+++ db/seeds.rb
@@ -288,16 +288,16 @@ end
       )
     end
     i = 1
+    a = 1
 150.times do
   Image.create!(
   file_id: ['1687654e4eb2d536fe0d59de264c6244b92ff03da25e32f38eb94e26e539','04c86fb9b07208fcea64889aea5059d7c4939b5a3f5ba5edf7f00f313b9f','15a33afca32c3f5b59b8035b17c324f198a76919bb09105dc3035a458a4d','f8ed69e4e3bcce539e368bf9509f1389c4afc0d3d5af6e37ada037a50c95'].sample,
-  board_id: i,
+  board_id: a,
   )
-  if i < 80
-    i += 1
-  else
-    i -= 1
+  if i % 2 == 0
+  a += 1
   end
+  i += 1
 end
 
   i = 1
@@ -306,11 +306,8 @@ end
   file_id: ['1687654e4eb2d536fe0d59de264c6244b92ff03da25e32f38eb94e26e539','04c86fb9b07208fcea64889aea5059d7c4939b5a3f5ba5edf7f00f313b9f','15a33afca32c3f5b59b8035b17c324f198a76919bb09105dc3035a458a4d','f8ed69e4e3bcce539e368bf9509f1389c4afc0d3d5af6e37ada037a50c95'].sample,
   accessory_id: i
   )
-  if i < 80
     i += 1
-  else
-    i -= 1
-  end
+
 end
 
 
