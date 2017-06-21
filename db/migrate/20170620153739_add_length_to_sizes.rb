612df22774b4ab27c70e478f331d7f898677b815
Ryan Kliemann <Ryan_blake@icloud.com>
2017-06-21 10:44:24 -0500

    floats:size

--- db/migrate/20170620153739_add_length_to_sizes.rb
+++ db/migrate/20170620153739_add_length_to_sizes.rb
@@ -1,8 +1,12 @@
 class AddLengthToSizes < ActiveRecord::Migration[5.0]
   def change
-    add_column :sizes, :length, :integer
-    add_column :sizes, :width, :integer
-    add_column :sizes, :thickness, :integer
-    add_column :sizes, :volume, :integer
+    add_column :sizes, :length, :decimal,:scale => 2
+
+    add_column :sizes, :width, :decimal,:scale => 2
+
+    add_column :sizes, :thickness, :decimal,:scale => 2
+
+    add_column :sizes, :volume, :decimal, :scale => 2
+
   end
 end
--- db/migrate/20170620182854_add_unit_to_accessories.rb
+++ db/migrate/20170620182854_add_unit_to_accessories.rb
@@ -1,6 +1,6 @@
 class AddUnitToAccessories < ActiveRecord::Migration[5.0]
   def change
     add_reference :accessories, :unit, foreign_key: true, index: true
-    add_column :accessories, :measure, :integer
+    add_column :accessories, :measure, :decimal
   end
 end
