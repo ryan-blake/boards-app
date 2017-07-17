566fb73952c22e95709dcff5126e2b4e732aaf5e
Ryan Kliemann <Ryan_blake@icloud.com>
2017-07-17 16:46:26 -0500

    full_Address_charge

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,34 +1,4 @@
 # == Schema Information
-#
-# Table name: charges
-#
-#  id            :integer          not null, primary key
-#  price         :integer
-#  item          :string
-#  user_id       :integer
-#  vendor_id     :integer
-#  token         :string
-#  customer_id   :string
-#  completed     :boolean          default("f")
-#  boolean       :boolean          default("f")
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  board_id      :string
-#  address       :string
-#  shipping      :boolean
-#  start_time    :datetime
-#  end_time      :datetime
-#  rental        :boolean
-#  charge_stripe :string
-#  accessories   :string
-#  picked        :string
-#  shipped       :string
-#  zipcode       :string
-#  country       :string
-#  state         :string
-#  city          :string
-#
-
 # Table name: charges
 #
 #  id            :integer          not null, primary key
@@ -59,6 +29,9 @@ class Charge < ApplicationRecord
   belongs_to :board
   belongs_to :vendor, class_name: 'User', foreign_key: 'vendor_id'
 
+  def full_address
+    [address, city, state, zipcode].join(', ')
+  end
 
   def rent_time(board)
     board.end_time - board.start_time
