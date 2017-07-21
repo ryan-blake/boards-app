fe369c1562857a252b0da3f8fb9c1676bcd16e21
Ryan Kliemann <Ryan_blake@icloud.com>
2017-07-21 13:11:37 -0500

    charge.price

--- db/migrate/20161209181631_create_charges.rb
+++ db/migrate/20161209181631_create_charges.rb
@@ -1,7 +1,6 @@
 class CreateCharges < ActiveRecord::Migration[5.0]
   def change
     create_table :charges do |t|
-      t.integer :price
       t.string :item
       t.references :user, index: true, foreign_key: true
       t.integer :vendor_id
