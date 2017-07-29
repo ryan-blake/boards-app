0a7d9180858985bf19562b06f3d0b0dcbb3638a4
Ryan Kliemann <Ryan_blake@icloud.com>
2017-07-29 13:16:36 -0500

    prefix https:

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative 'boot'
 require 'csv'
 require 'rails/all'
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,6 +13,5 @@ module RailsDevise
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # config.assets.initialize_on_precompile = false
-
   end
 end
